745f22d396f797ab8215de324249dbf50164c815
Sharik Dhiman <dhimansharik@gmail.com>
2026-07-16 23:32:17 +0530

    Add files via upload

---
+++ ass1.java
@@ -0,0 +1,23 @@
+public class ass1 {
+    // Method
+    static int square(int number)
+    {
+        return number * number;
+    }
+
+    public static void main(String[] args)
+    {
+        // Variables
+        String name = "phil";
+        double salary = 55000.50;
+        int age = 43;
+
+        int result = square(6);
+
+        System.out.println("name: " + name);
+        System.out.println("age: " + age);
+        System.out.println("salary: " + salary);
+        System.out.println("square of 6: " + result);
+    }
+}
+
